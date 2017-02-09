6f9a77e66045ddda60177f1dac9597ca4c3e8fbf
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-02-08 19:39:47 -0500

    day 3challenge using css variables

---
+++ Day_3/script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+function updateHandler() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+
+  console.log(this.name);
+}
+inputs.forEach(input => input.addEventListener('change', updateHandler));
+inputs.forEach(input => input.addEventListener('mousemove', updateHandler));
